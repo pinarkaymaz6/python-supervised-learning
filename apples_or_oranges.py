66c078551bb3a25ae6ce5fe8dba4009723557df5
Pınar Kaymaz <pinarkaymaz6@gmail.com>
2017-08-20 21:47:26 +0300

    Add files via upload

---
+++ apples_or_oranges.py
@@ -0,0 +1,10 @@
+import sklearn
+from sklearn import tree
+features = [[140,1],[130,1],[150,0],[170,0]]
+labels = [0,0,1,1]
+clf = tree.DecisionTreeClassifier()
+clf = clf.fit(features, labels)
+print ("the guess: "+str(clf.predict([[160,0]])))
+result = clf.predict([[160,0]])
+if result[0]==1:
+	print("correct")
